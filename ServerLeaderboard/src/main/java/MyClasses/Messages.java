0e3fd44a58355154fb26caa8b1cc55e2b3c7b1b0
unknown <omkob@hotmail.com>
2021-11-29 13:31:21 +0100

    Messages class for displaying the needed error, success or whatever messages

---
+++ ServerLeaderboard/src/main/java/MyClasses/Messages.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package MyClasses;
+
+/**
+ *
+ * @author Omer
+ */
+public class Messages {
+    public static final String MESSAGE_ERROR_TOO_MANY_CONTESTANTS = "That's a huge number, lower it.";
+    public static final String MESSAGE_ERROR_MUST_BE_ABOVE_ZERO = "Can't be zero or less.";
+}
